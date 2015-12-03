b908321c0786f5fd16a1d7c8304aa5530bc48df1
Roy Hyunjin Han <starsareblueandfaraway@gmail.com>
2015-12-03 11:55:53 -0500

    Decide demographic naming convention

--- infrastructure_planning/demography/linear.py
+++ infrastructure_planning/demography/linear.py
@@ -1,9 +1,5 @@
 import numpy as np
-from argparse import ArgumentParser
 from collections import defaultdict
-from crosscompute_table import TableType
-from invisibleroads_macros.disk import make_enumerated_folder_for, make_folder
-from invisibleroads_macros.log import format_summary
 from math import ceil
 from os.path import join
 from pandas import DataFrame, concat
@@ -92,32 +88,3 @@ def get_future_years(target_year, year_packs):
     if target_year not in past_years:
         future_years.append(target_year)
     return future_years
-
-
-if __name__ == '__main__':
-    argument_parser = ArgumentParser()
-    argument_parser.add_argument(
-        '--target_folder', metavar='FOLDER', type=make_folder)
-    argument_parser.add_argument(
-        '--population_table_path', metavar='PATH', required=True)
-    argument_parser.add_argument(
-        '--name_column', metavar='COLUMN', required=True)
-    argument_parser.add_argument(
-        '--population_column', metavar='COLUMN', required=True)
-    argument_parser.add_argument(
-        '--year_column', metavar='COLUMN', required=True)
-    argument_parser.add_argument(
-        '--target_year', metavar='YEAR', type=int, required=True)
-    argument_parser.add_argument(
-        '--yearly_population_growth_percent', metavar='PERCENT', type=float,
-        required=True)
-    args = argument_parser.parse_args()
-    d = forecast_population(
-        args.target_folder or make_enumerated_folder_for(__file__),
-        TableType().load(args.population_table_path),
-        args.name_column,
-        args.population_column,
-        args.year_column,
-        args.target_year,
-        args.yearly_population_growth_percent)
-    print(format_summary(d))
--- infrastructure_planning/electricity/consumption/linear.py
+++ infrastructure_planning/electricity/consumption/linear.py
@@ -1,16 +1,11 @@
-from argparse import ArgumentParser
-from crosscompute_table import TableType
-from invisibleroads_macros.disk import make_enumerated_folder_for, make_folder
-from invisibleroads_macros.log import format_summary
-
-from ..demography import forecast_population
+# from ..demography import forecast_population
 
 
 def run(
         target_folder, population_table,
         name_column, population_column, year_column,
         target_year, yearly_population_growth_percent,
-        electricity_consumption_in_kwh_per_capita_table):
+        electricity_consumption_per_capita_table):
 
     # get future population
     # get future consumption in kWh per capita
@@ -20,37 +15,3 @@ def run(
 
     return [
     ]
-
-
-if __name__ == '__main__':
-    argument_parser = ArgumentParser()
-    argument_parser.add_argument(
-        '--target_folder', metavar='FOLDER', type=make_folder)
-    argument_parser.add_argument(
-        '--population_table_path', metavar='PATH', required=True)
-    argument_parser.add_argument(
-        '--name_column', metavar='COLUMN', required=True)
-    argument_parser.add_argument(
-        '--population_column', metavar='COLUMN', required=True)
-    argument_parser.add_argument(
-        '--year_column', metavar='COLUMN', required=True)
-    argument_parser.add_argument(
-        '--target_year', metavar='YEAR', type=int, required=True)
-    argument_parser.add_argument(
-        '--yearly_population_growth_percent', metavar='PERCENT', type=float,
-        required=True)
-    argument_parser.add_argument(
-        '--electricity_consumption_in_kwh_per_capita_table_path',
-        metavar='PATH', required=True)
-    args = argument_parser.parse_args()
-    d = run(
-        args.target_folder or make_enumerated_folder_for(__file__),
-        TableType().load(args.population_table_path),
-        args.name_column,
-        args.population_column,
-        args.year_column,
-        args.target_year,
-        args.yearly_population_growth_percent,
-        TableType().load(
-            args.electricity_consumption_in_kwh_per_capita_table_path))
-    print(format_summary(d))
