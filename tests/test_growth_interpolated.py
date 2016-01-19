edaac29d34d25a45cf177c15c688b272a4f6c6b2
Roy Hyunjin Han <starsareblueandfaraway@gmail.com>
2016-01-19 14:07:01 -0500

    Prepare to separate branch

---
+++ tests/test_growth_interpolated.py
@@ -0,0 +1,7 @@
+from infrastructure_planning.growth.interpolated import (
+    get_interpolated_spline_extrapolated_linear_function)
+
+
+def test_get_interpolated_spline_extrapolated_linear_function():
+    # If x is inside the interpolated domain, y should match original
+    pass
