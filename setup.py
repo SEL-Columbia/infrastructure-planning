2ff9072c2dbbd56db3defa65b750beb5851c7d3a
Roy Hyunjin Han <starsareblueandfaraway@gmail.com>
2016-07-11 22:00:10 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='infrastructure-planning',
-    version='2.0.7',
+    version='2.0.8',
     description='Infrastructure planning tools',
     long_description=DESCRIPTION,
     classifiers=[
