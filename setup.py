5f5cfbc9af51d365744c228a32b025bbd3741bcf
Roy Hyunjin Han <rhh@crosscompute.com>
2016-10-15 14:35:54 -0400

    Bump version after discussion

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='infrastructure-planning',
-    version='2.2.0',
+    version='3.0.0',
     description='Infrastructure planning tools',
     long_description=DESCRIPTION,
     classifiers=[
