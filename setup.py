fb7cd343c9a8900c27c26f07a2bba07bebb1b81a
Roy Hyunjin Han <rhh@crosscompute.com>
2016-11-22 22:24:27 -0500

    Update dependencies

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     ],
     install_requires=[
         # 'GDAL',
-        'geometryIO>=0.9.7.2',
+        'geometryIO>=0.9.7.3',
         'geopy',
         'invisibleroads-macros>=0.7.4',
         'matplotlib',
