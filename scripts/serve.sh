2f5a619cf26c40d6a1cd81c7c52cf017587c7cbb
Roy Hyunjin Han <rhh@crosscompute.com>
2016-12-23 11:24:40 -0500

    Add scripts

---
+++ scripts/serve.sh
@@ -0,0 +1,5 @@
+source ~/.virtualenvs/crosscompute/bin/activate
+crosscompute serve \
+    estimate-electricity-cost-by-technology-from-population \
+    --website_name "Sustainable Engineering Lab of Columbia University" \
+    --website_owner "Sustainable Engineering Lab of Columbia University"
